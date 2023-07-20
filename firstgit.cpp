39c71743c7a3c14de1f4e526aefa99c408d512f7
Irfan <irfaninfinity007@gmail.com>
2023-07-20 17:08:11 +0530

    this is first.cpp

--- firstgit.cpp
+++ firstgit.cpp
@@ -4,6 +4,7 @@ int main(){
     int a=5;
     int b= 6;
     int sum = a+b;
-    cout<<"the sum is :"<<sum;
+    cout<<"the sum is :"<<sum<<endl;
+    cout<<"the product is :"<<a*b;
     return 0;
 }
