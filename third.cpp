0fc0db87680ed0e7c23a1a260403ec30a89d218a
Irfan <irfaninfinity007@gmail.com>
2023-07-20 19:35:27 +0530

    this

---
+++ third.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int b= 6;
+    int sum = a+b;
+    cout<<"the sum is :"<<sum<<endl;
+    cout<<"the product is :"<<a*b;
+    return 0;
+}
